e479760c8f06ba60e39bdd3938cc359b7f38be94
Kevin Utoft <kevin.utoft@gmail.com>
2015-11-09 23:23:47 -0600

    update
    add resume
    update overview to carousel

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -34,11 +34,87 @@ $(document).ready(function () {
         return false;
     });
      $(".project-back").on("click", function(){
-         var $content = $(this).parent().parent();
+         var $content = $(this).parent().parent().parent();
          $($content).removeClass("show");
          return false;
     });
     
+    $(".project-next").on("click", function(){
+        var $num = $(this).parent().parent().parent().parent().data("id");
+        var $next = "#project-" + ($num + 1) + "-content";
+        $(this).parent().parent().parent().removeClass("show");
+        $($next).addClass("show");
+        return false;
+    });
+    $(".project-prev").on("click", function(){
+        var $num = $(this).parent().parent().parent().parent().data("id");
+        var $prev = "#project-" + ($num - 1) + "-content";
+        $(this).parent().parent().parent().removeClass("show");
+        $($prev).addClass("show");
+        return false;
+    });
+    
+    $('.owl-carousel').owlCarousel( {
+        singleItem: true,
+        autoPlay: 6000,
+        lazyLoad: true,
+    }  
+    );
+    
+    var cardSlider = function(c) {
+        var $sWrapper = $(c).children(".slides-wrapper");
+        var $slides = $sWrapper.children(".slide");
+        var $controls = $(c).children(".controls");
+        var $h = $(".mod-carousel-wrapper").children(".slides-wrapper").children(".slide").height() + "px";
+        
+        $sWrapper.css("height", $h);
+        $slides.attr("style", function(i) {
+            
+            var z = 20 - i,
+                t = (-i * 30) + "px";
+            
+            var string = "z-index:" + z + "; top:" + t + ";";
+            return string;
+        });
+        $slides.each(function() {
+            $controls.append("<li><a href='#' class='page'></a></li>");
+        });
+    };
+    cardSlider(".mod-carousel-wrapper");
+    
+    $(".mod-carousel-wrapper .controls .next").on("click", function() {
+        if ( $(".slide:last-child").hasClass("active") ) {
+            return false;
+        } else {
+            $(".slide").each(function() {
+                $(this).css("top", "+=30");
+            });
+            $(".slide").removeClass("second");
+            $(".slide").removeClass("third");
+            $(".slide.active").removeClass("active").next().addClass("active").removeClass("off");
+            $(".slide.active").prev().addClass("off");
+            $(".slide.active").next().addClass("second").removeClass("off");
+            $(".slide.active").next().next().addClass("third").removeClass("off");
+
+            return false;
+        }
+    });
+    $(".mod-carousel-wrapper .controls .prev").on("click", function() {
+        if ( $(".slide:first-child").hasClass("active") ) {
+            return false;
+        } else {
+            $(".slide").removeClass("second");
+            $(".slide").removeClass("third");
+            $(".slide.active").removeClass("active").prev().addClass("active").removeClass("off");
+            $(".slide").each(function() {
+                $(this).css("top", "-=30");
+            });
+            $(".slide.active").prev().addClass("off");
+            $(".slide.active").next().addClass("second").removeClass("off");
+            $(".slide.active").next().next().addClass("third").removeClass("off");
+            return false;
+        }
+    });
     
     // =====
     // Execute this JS after page content has loaded (images, videos, etc):
